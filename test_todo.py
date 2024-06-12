637d1b2a871ad410ec7aa58805995831e0e484f9
future-genius16 <elyaibragimova16@gmail.com>
2024-06-12 16:51:15 +0300

    Исправлены мини ошибки в test_todo.py

--- test_todo.py
+++ test_todo.py
@@ -15,14 +15,14 @@ class TestTodoList(unittest.TestCase):
     def test_remove_task(self):
         self.todo_list.add_task("Task 1")
         self.todo_list.add_task("Task 2")
-        self.todo_list.remove_task(1)
+        self.todo_list.delete_task(1)
         self.assertEqual(len(self.todo_list.tasks), 1)
         self.assertEqual(self.todo_list.tasks[0], "Task 1")
 
     def test_show_tasks(self):
         self.todo_list.add_task("Task 1")
         self.todo_list.add_task("Task 2")
-        tasks = self.todo_list.show_tasks()
+        tasks = self.todo_list.list_tasks()
         self.assertEqual(tasks, ["Task 1", "Task 2"])
 
 
