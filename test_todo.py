201dba137dcda97406543479320001254b196cbb
future-genius16 <elyaibragimova16@gmail.com>
2024-06-12 17:06:01 +0300

    Исправлен test_todo.py

--- test_todo.py
+++ test_todo.py
@@ -15,7 +15,7 @@ class TestTodoList(unittest.TestCase):
     def test_remove_task(self):
         self.todo_list.add_task("Task 1")
         self.todo_list.add_task("Task 2")
-        self.todo_list.delete_task("Task 2")
+        self.todo_list.delete_task(1)
         self.assertEqual(len(self.todo_list.tasks), 1)
         self.assertEqual(self.todo_list.tasks[0], "Task 1")
 
