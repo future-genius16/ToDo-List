e2b38ec831e448231fb964ab17a111f568d86128
future-genius16 <elyaibragimova16@gmail.com>
2024-06-12 17:33:26 +0300

    Добавлена функция "пометить задание как выполненное"

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def main():
         print("\n1. Add task")
         print("2. Remove task")
         print("3. Show tasks")
-        print("4. Exit")
+        print("4. Mark task as done")
+        print("5. Exit")
 
         choice = input("\nEnter your choice: ")
 
@@ -16,15 +17,18 @@ def main():
             todo_list.add_task(task)
         elif choice == '2':
             index = int(input("Enter task number to remove: "))
-            todo_list.remove_task(index)
+            todo_list.remove_task(index-1)
         elif choice == '3':
-            tasks = todo_list.show_tasks()
+            tasks = todo_list.list_tasks()
             for idx, task in enumerate(tasks, start=1):
                 print(f"{idx}. {task}")
         elif choice == '4':
+            index = int(input("Enter task number to mark as done: "))
+            todo_list.mark_task_done(index - 1)
+        elif choice == '5':
             break
         else:
-            print("Invalid choice. Please enter a number between 1 and 4.")
+            print("Invalid choice. Please enter a number between 1 and 5.")
 
 
 if __name__ == "__main__":
